75a0760eb05e3e83f1ae2fcb71b9f0e4fee79288
ninatodic <man.who.wants.to.fly@gmail.com>
2021-03-22 15:23:40 +0100

    preparing to deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //Connect Database
@@ -10,7 +10,16 @@ connectDB();
 //Init midleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.json({ msg: 'Hello to expence tracker app' }));
+//Serve static assets in production
+
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 //Define routes
 
