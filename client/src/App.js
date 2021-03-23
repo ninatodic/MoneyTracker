cdd3377682dd0d68b20c49ce0dd98ed636d0ad4d
ninatodic <man.who.wants.to.fly@gmail.com>
2021-03-23 10:47:17 +0100

    one last try

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 import './App.css';
